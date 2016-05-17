5cec6bd310500ba2579281f798953b5ff0780cce
Jordan Patterson <jordan@jpatterson.me>
2016-05-17 12:27:08 -0300

    a shot in the dark

--- lib/bitbucket_rest_api/connection.rb
+++ lib/bitbucket_rest_api/connection.rb
@@ -44,7 +44,8 @@ module BitBucket
         builder.use Faraday::Request::UrlEncoded
         builder.use FaradayMiddleware::OAuth, {:consumer_key => client_id, :consumer_secret => client_secret, :token => oauth_token, :token_secret => oauth_secret} if client_id? and client_secret?
         builder.use BitBucket::Request::BasicAuth, authentication if basic_authed?
-        builder.use FaradayMiddleware::EncodeJson
+        # builder.use FaradayMiddleware::EncodeJson
+        builder.use BitBucket::Request::Jsonize
 
         builder.use Faraday::Response::Logger if ENV['DEBUG']
         builder.use BitBucket::Response::Helpers
